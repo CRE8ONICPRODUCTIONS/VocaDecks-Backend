89e82c47c5ac76939610ba46856da0368598ebfd
CRE8ONICPRODUCTIONS <williamnewberr10@gmail.com>
2025-04-10 02:19:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes'); // Ensure the path is correct
+
 
 
 // Load environment variables from .env file
